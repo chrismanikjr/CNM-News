23cad9256a8abd0eeb3e1b53e53592a24a856f0a
Discovery Pede <chris.manikjr@gmail.com>
2021-06-23 15:51:55 +0700

    Initial Commit

---
+++ CNM-News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CNM-News
+//
+//  Created by Chrismanto Manik on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CNM-NewsTests/CNM_NewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  CNM_NewsTests.swift
+//  CNM-NewsTests
+//
+//  Created by Chrismanto Manik on 6/23/21.
+//
+
+import XCTest
+@testable import CNM_News
+
+class CNM_NewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
